d4582993d90dc125239c37d4573619313c8c6236
Christina Lyu <clyu@clyu-macbookpro.roam.corp.google.com>
2019-05-28 18:07:41 -0700

    delete

--- foo/HelloWorld.java
+++
@@ -1,5 +0,0 @@
-public class HelloWorld {
-  public void say(String testInput) {
-    System.out.println(testInput + " test completed");
-  }
-}
--- testJavaApplet.java
+++
@@ -1,9 +0,0 @@
-import java.applet.*;
-import java.awt.*;
-public class testJavaApplet extends Applet
-{
- public void paint(Graphics g)
- {
- g.drawString("this is code from java",10,10);
- }
-}
